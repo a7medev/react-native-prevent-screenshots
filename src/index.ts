a312fde8005ed61dcbcfd600eeae58dcd34159cb
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-10-23 13:06:48 +0200

    chore: check for linking

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { NativeModules } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
 
-const { PreventScreenshot } = NativeModules;
+const LINKING_ERROR =
+  `The package 'react-native-prevent-screenshot' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const PreventScreenshot = NativeModules.PreventScreenshot
+  ? NativeModules.PreventScreenshot
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 export default PreventScreenshot;
